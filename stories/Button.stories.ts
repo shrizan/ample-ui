596ea58665f9f6876854b8328cbc348400c79aa1
Shreejan Acharya <shreejan.acharya@cnspartner.com>
2024-02-05 22:10:18 -0500

    button component added

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -20,29 +20,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const MyButton: Story = {
   args: {
     label: "Button",
     type: "primary",
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: "Button",
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: "small",
-    label: "Button",
   },
 };
--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,19 +1,17 @@
+import "./button.css";
 export type ButtonType = "primary" | "outline" | "default" | "dashed" | "text" | "link";
 
-const SIZE_TO_CLASS = {
-  "small": ' py-2 px-10 rounded-button ',
-  "medium": ' py-3 px-15 rounded-button ',
-  "large": ' py-4 px-20 rounded-button  '
-}
-
-function getClassOnType(type: ButtonType) {
-  if (type == "primary") {
-    return 'bg-primary border-0 color-red';
-  }
-  else {
-    return 'text-black';
-  }
-}
+const BUTTON_TYPE_TO_CLASS = {
+  "primary": "btn-primary",
+  "outline": "btn-outline",
+  "default": "",
+  "dashed": "btn-dashed",
+  "text": "btn-text",
+  "link": "btn-link",
+  "small": "btn-sm",
+  "medium": "btn-md",
+  "large": "btn-lg"
+};
 
 interface ButtonProps {
   type?: ButtonType;
@@ -45,36 +43,18 @@ export const Button = ({
   label,
   ...props
 }: ButtonProps) => {
+  const btnClass = ['btn'];
+  const btnType = BUTTON_TYPE_TO_CLASS[type];
+  btnClass.push(BUTTON_TYPE_TO_CLASS[type])
+  if (danger) btnClass.push(btnType + '-danger');
+  btnClass.push(BUTTON_TYPE_TO_CLASS[size]);
+  return (
+    <button
+      className={btnClass.join(' ')}
+      {...props}
+    >
+      {label}
+    </button>
+  );
 
-  if (type == "primary") {
-    return (
-      <button
-        className="rounded-md px-7 py-2 bg-primary text-white hover:bg-primary-300 duration-100"
-        {...props}
-      >
-        {label}
-      </button>
-    );
-  }
-  else {
-    return (
-      <button
-        className="rounded-md px-7 py-2 border border-zinc-400 text-zinc-600 hover:border-primary duration-100 hover:text-primary"
-        {...props}
-      >
-        {label}
-      </button>
-    );
-  }
-
-
-  // return (
-  //   <button
-  //     type="button"
-  //     className={[SIZE_TO_CLASS[size], getClassOnType(type)].join(' ')}
-  //     {...props}
-  //   >
-  //     {label}
-  //   </button>
-  // );
-};
+}
